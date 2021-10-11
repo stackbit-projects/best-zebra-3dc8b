2c3a0253e1b1fb751b11e897770e60705bfc0463
Stackbit Code Editor <projects@stackbit.com>
2021-10-11 06:44:41 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -16,7 +16,7 @@ export default class HeroSection extends React.Component {
             <section id={sectionId} className="hero">
                 <div className="container container--lg">
                     {title && <h1 className="hero__title">{title}</h1>}
-                    {content}
+                    
                     {!_.isEmpty(actions) && (
                         <div className="hero__actions button-group">
                             <CtaButtons actions={actions} />
